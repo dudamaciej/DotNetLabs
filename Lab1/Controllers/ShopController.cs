d60d5de3af8983e94ee27b3e08c5052655036ccf
dudamaciej <maciekduda99@gmail.com>
2020-10-08 18:09:36 +0200

    second task added

---
+++ Lab1/Controllers/ShopController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using static DotNetLabs.Models.ShopProductModel;
+
+namespace DotNetLabs.Controllers
+{
+    public class ShopController : Controller
+    {
+
+        public IActionResult Index()
+        {
+            List<Product> products = new List<Product>();
+            products.Add(new Product() { Name = "Air Joran 1 Mid Shadow", Description = "size 42 color: black/white", Price = 699 });
+            products.Add(new Product() { Name = "Yeezy Boost 350v2", Description = "size 43 color: red", Price = 899 });
+            products.Add(new Product() { Name = "Sacai", Description = "size 45 color: nylon-white", Price = 1000 });
+            return View(products);
+        }
+    }
+}
---
+++ Lab1/Models/ShopProductModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace DotNetLabs.Models
+{
+    public class ShopProductModel
+    {
+        public class Product
+        {
+            public string Name { get; set; }
+            public string Description { get; set; }
+            public decimal Price { get; set; }
+
+         
+        }
+    }
+}
