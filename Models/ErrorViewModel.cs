950cd1ccebec6e002692754a6c58356de182bf1c
dudamaciej <maciekduda99@gmail.com>
2020-10-08 13:28:40 +0200

    default setup

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace DotNetLabs.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
