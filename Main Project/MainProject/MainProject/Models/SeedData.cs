c75a93982644ddad0361ab8647782bb9173866bf
dudamaciej <maciekduda99@gmail.com>
2020-10-27 15:12:04 +0100

    solved namespace issue

--- Main Project/MainProject/MainProject/Models/SeedData.cs
+++ Main Project/MainProject/MainProject/Models/SeedData.cs
@@ -7,7 +7,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
-namespace WebApplication9.Models
+namespace MainProject.Models
 {
     public class SeedData
     {
