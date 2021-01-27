cc5462b63493191b6875d7edfbe72c1a8ea37a08
dudamaciej <maciekduda99@gmail.com>
2021-01-27 13:21:23 +0100

    counter fixed

--- Main Project/MainProject/MainProject/wwwroot/lib/js/counter.js
+++ Main Project/MainProject/MainProject/wwwroot/lib/js/counter.js
@@ -1,10 +1,10 @@
 ﻿"use strict"
 
-var connection = new signalR.HubConnectionBuilder().withUrl("/counterHub").build();
+var counterconnection = new signalR.HubConnectionBuilder().withUrl("/counterHub").build();
 
-connection.on("updateCount", function (Count) {
+counterconnection.on("updateCount", function (Count) {
     var text = document.getElementById("userOnlineBox");
     text.innerHTML = `Online Users: ${Count}`;
 });
 
-connection.start();
+counterconnection.start();
